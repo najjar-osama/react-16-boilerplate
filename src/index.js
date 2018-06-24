34b2bbaf5682fd3f10610168d589a647c8024d90
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-06-24 02:04:50 +0200

    added warning message to add .env.* files to .gitignore files after starting real development

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-//check
-//console.log(process.env.REACT_APP_SECRET_CODE);
+//please not that REACT_APP* prefix is madnatory otherwise the env vraibale will be ignored.
+console.log(`I am now running in ${process.env.REACT_APP_SECRET_CODE}`);
+console.warn(
+  "Add your .env.* files to .gitignore as these files are not added by default, in this boilerplate"
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
