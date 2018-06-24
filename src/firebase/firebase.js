193115c5c6825b10290283959dcc134b39f70a3e
Osama Mohammad Najjar <osama.najjar.m@gmail.com>
2018-06-24 03:04:34 +0200

    setting firebase auth & datbase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+
+// These imports load individual services into the firebase namespace.
+import "firebase/auth";
+import "firebase/database";
+
+const conf = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(conf);
+
+export const database = firebase.database;
+export const auth = firebase.auth;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "normalize.css";
 import "./styles/styles.css";
 import App from "./App";
 
+import { database } from "./firebase/firebase";
+
 import registerServiceWorker from "./registerServiceWorker";
 
 //please not that REACT_APP* prefix is madnatory otherwise the env vraibale will be ignored.
@@ -11,6 +13,10 @@ console.log(`I am now running in ${process.env.REACT_APP_SECRET_CODE}`);
 console.warn(
   "Add your .env.* files to .gitignore as these files are not added by default, in this boilerplate"
 );
+
+database()
+  .ref()
+  .set({ name: "osama" });
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
